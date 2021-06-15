bf95ffb287f03dd7f06b3f86b9bc2c777c6d4e15
berks44 <43846785+berks44@users.noreply.github.com>
2021-06-15 19:39:15 +0300

    Add files via upload

---
+++ DenemeApi/Repositories/IBookRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DenemeApi.Models;
+
+namespace DenemeApi.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> Get();
+        Task<Book> Get(int id);
+        Task<Book> Create(Book book);
+        Task Update(Book book);
+        Task Delete(int id);
+    }
+}
